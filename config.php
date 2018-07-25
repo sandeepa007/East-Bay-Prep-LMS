5063f77cb2b04650b1c842ee44ce9fcb2a2ac820
Kha Team <sandeepa@khacreation.com>
2018-07-25 15:56:48 +0530

    Default Moodle with Signup Page patch

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'lms-east-bay';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'veblogy';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_bin',
+);
+
+$CFG->wwwroot   = 'http://219.91.251.250/lms';
+$CFG->dataroot  = '/var/www/lmsdata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
